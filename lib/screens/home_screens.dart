142da596082ce838eaedb02e638366a303a63b13
Aldhisaputra <aldhisaputra795@gmail.com>
2025-05-19 21:15:08 +0700

    model data task

--- lib/screens/home_screens.dart
+++ lib/screens/home_screens.dart
@@ -1,31 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/models/task.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<Task> tasks = [
+    Task(title: "Buy Groceries"),
+    Task(title: "Finish Flutter Project"),
+    Task(title: "Workout 30 Minutes"),
+    Task(title: "Workout 30 Minutes", isDone: true),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("My Tasks"),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
-      ), // AppBar
-      body: ListView(
-        children: [
-          ListTile(
-            leading: Icon(Icons.check_box_outline_blank),
-            title: Text("Buy Groceries"),
-          ), // ListTile
-          ListTile(
-            leading: Icon(Icons.check_box),
-            title: Text("Finish Flutter Project"),
-          ), // ListTile
-          ListTile(
-            leading: Icon(Icons.check_box_outline_blank),
-            title: Text("Workout 30 Minutes"),
-          ), // ListTile
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add),
+          ),
         ],
-      ), // ListView
-    ); // Scaffold
+      ),
+      body: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          final task = tasks[index];
+          return ListTile(
+            leading: Icon(
+              task.isDone ? Icons.check_box : Icons.check_box_outline_blank,
+            ),
+            title: Text(task.title),
+          );
+        },
+      ),
+    );
   }
 }
